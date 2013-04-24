3ad82768e305c7415e91dc2cd0bb187af023cdf3
Kevin Kirsche <kkirsche@hsc.edu>
2013-04-24 15:10:18 -0400

    Switch second video to display first

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
         $(".videoWrapper").fadeOut("fast");
         switch (videoNumber) {
             case 1:
-                newVideoTitle = "Dr. Pontuso supports 3,000 Strong Challenge";
-                newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/eYhsyjCU5-s?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
-                break;
-            case 2:
                 newVideoTitle = "Life on Campus: Spring Semester 2013";
                 newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/XK93kVGLZ6Y?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
+                break;
+            case 2:
+                newVideoTitle = "Dr. Pontuso supports 3,000 Strong Challenge";
+                newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/eYhsyjCU5-s?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
                 break;
             default:
                 newVideoTitle = "";
