e00e21a536dbbb2e29cd51167772b19d870948c4
Kevin Kirsche <kkirsche@hsc.edu>
2013-05-24 10:27:57 -0400

    Updates per Spencer's Requests
    Cheryle & Kevin—
    
    We would like to send our next video out this morning. Could you please add the new video of Dr. Cabas and Townsend which can be found at http://www.youtube.com/hampdensydneycollege to the videos on 3000strong.hsc.edu? The link above is not the direct link to the specific video.
    
    Please title the video: “Fishin’ Blues” by Professors Cabas and Townsend
    
    I would like to change the text for the status update area. Let’s make it “Every new alumni donor brings us closer to 3,000.”
    
    These are the only changes I have at this time.

--- js/main.js
+++ js/main.js
@@ -7,14 +7,18 @@ $(document).ready(function () {
         $(".videoWrapper").fadeOut("fast");
         switch (videoNumber) {
             case 1:
+                newVideoTitle = "&ldquo;Fishin&rsquo; Blues&rdquo; by Professors Cabas and Townsend";
+                newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/MQKjobmu-0Y?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
+                break;
+            case 2:
                 newVideoTitle = "General Sam Shares a Story";
                 newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/_gRmUAqaCwk?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
                 break;
-            case 2:
+            case 3:
                 newVideoTitle = "Life on Campus: Spring Semester 2013";
                 newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/XK93kVGLZ6Y?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
                 break;
-            case 3:
+            case 4:
                 newVideoTitle = "Dr. Pontuso supports 3,000 Strong Challenge";
                 newVideoURL = "<iframe width=\"521\" height=\"293\" src=\"http://www.youtube.com/embed/eYhsyjCU5-s?rel=0&amp;modestbranding=1&amp;showinfo=0\" frameborder=\"0\" allowfullscreen></iframe>";
                 break;
@@ -30,7 +34,7 @@ $(document).ready(function () {
     }
     $("[name=nextVideo]").click(function(e) {
         e.preventDefault();
-        if (whichVideoIsNext > 3) {
+        if (whichVideoIsNext > 4) {
             whichVideoIsNext = 1;
         } else if (whichVideoIsNext < 1) {
             whichVideoIsNext = 1;
@@ -41,10 +45,10 @@ $(document).ready(function () {
     $("[name=prevVideo]").click(function(e) {
         e.preventDefault();
         whichVideoIsNext -= 2;
-        if (whichVideoIsNext > 3) {
-            whichVideoIsNext = 3;
+        if (whichVideoIsNext > 4) {
+            whichVideoIsNext = 4;
         } else if (whichVideoIsNext < 1) {
-            whichVideoIsNext = 3;
+            whichVideoIsNext = 4;
         }
         switchVideos(whichVideoIsNext);
         whichVideoIsNext++;
